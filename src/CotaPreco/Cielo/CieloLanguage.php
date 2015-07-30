7ea200ba8e52a6990e06d05043f3161dea911a29
Andrey <andreykvital@gmail.com>
2015-07-30 17:14:24 -0300

    Add CieloLanguage

---
+++ src/CotaPreco/Cielo/CieloLanguage.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace CotaPreco\Cielo;
+
+/**
+ * @author Andrey K. Vital <andreykvital@gmail.com>
+ */
+interface CieloLanguage
+{
+    /**
+     * @var string
+     */
+    const PORTUGUESE = 'PT';
+
+    /**
+     * @var string
+     */
+    const SPANISH = 'ES';
+
+    /**
+     * @var string
+     */
+    const ENGLISH = 'EN';
+}
