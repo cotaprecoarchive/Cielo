f281bc71aca2e927e474d869ffb603bce4240b46
Andrey <andreykvital@gmail.com>
2015-07-30 17:11:39 -0300

    Add PaymentMethod

---
+++ src/CotaPreco/Cielo/PaymentMethod.php
@@ -0,0 +1,128 @@
+<?php
+
+/*
+ * Copyright (c) 2015 Cota Preço
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace CotaPreco\Cielo;
+
+/**
+ * @author Andrey K. Vital <andreykvital@gmail.com>
+ */
+final class PaymentMethod
+{
+    /**
+     * @var CardIssuer
+     */
+    private $issuer;
+
+    /**
+     * @var string
+     */
+    private $product;
+
+    /**
+     * @var InstallmentsInterface
+     */
+    private $installments;
+
+    /**
+     * @param CardIssuer            $issuer
+     * @param string|int            $product
+     * @param InstallmentsInterface $installments
+     */
+    private function __construct(CardIssuer $issuer, $product, InstallmentsInterface $installments)
+    {
+        $this->issuer       = $issuer;
+        $this->product      = $product;
+        $this->installments = $installments;
+    }
+
+    /**
+     * @return CardIssuer
+     */
+    public function getCardIssuer()
+    {
+        return $this->issuer;
+    }
+
+    /**
+     * @return string|int
+     */
+    public function getProduct()
+    {
+        return $this->product;
+    }
+
+    /**
+     * @return OneTimePaymentInstallments|Installments
+     */
+    public function getInstallments()
+    {
+        return $this->installments;
+    }
+
+    /**
+     * @param  CardIssuer $issuer
+     * @return PaymentMethod
+     */
+    public static function forIssuerAsOneTimePayment(CardIssuer $issuer)
+    {
+        return new self($issuer, PaymentProduct::ONE_TIME_PAYMENT, new OneTimePaymentInstallments());
+    }
+
+    /**
+     * @param  CardIssuer $issuer
+     * @return PaymentMethod
+     */
+    public static function forIssuerAsDebitPayment(CardIssuer $issuer)
+    {
+        return new self($issuer, PaymentProduct::DEBIT, new OneTimePaymentInstallments());
+    }
+
+    /**
+     * @param  CardIssuer $issuer
+     * @param  int        $installments
+     * @return PaymentMethod
+     */
+    public static function forIssuerWithInstallmentsByMerchant(CardIssuer $issuer, $installments)
+    {
+        return new self(
+            $issuer,
+            PaymentProduct::INSTALLMENTS_BY_AFFILIATED_MERCHANTS,
+            Installments::fromNumberOfInstallments($installments)
+        );
+    }
+
+    /**
+     * @param  CardIssuer $issuer
+     * @param  int        $installments
+     * @return PaymentMethod
+     */
+    public static function forIssuerWithInstallmentsByCardIssuers(CardIssuer $issuer, $installments)
+    {
+        return new self(
+            $issuer,
+            PaymentProduct::INSTALLMENTS_BY_CARD_ISSUERS,
+            Installments::fromNumberOfInstallments($installments)
+        );
+    }
+}
---
+++ tests/CotaPreco/Cielo/PaymentMethodTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace CotaPreco\Cielo;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * @author Andrey K. Vital <andreykvital@gmail.com>
+ */
+class PaymentMethodTest extends TestCase
+{
+    /**
+     * @var CardIssuer
+     */
+    private $visa;
+
+    /**
+     * @var PaymentMethod
+     */
+    private $paymentMethod;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $this->visa          = CardIssuer::fromCreditCardType(CreditCardType::VISA);
+        $this->paymentMethod = PaymentMethod::forIssuerAsDebitPayment($this->visa);
+    }
+
+    /**
+     * @test
+     */
+    public function getCardIssuer()
+    {
+        $this->assertInstanceOf(CardIssuer::class, $this->paymentMethod->getCardIssuer());
+    }
+
+    /**
+     * @test
+     */
+    public function getProduct()
+    {
+        $this->assertSame(PaymentProduct::DEBIT, $this->paymentMethod->getProduct());
+    }
+
+    /**
+     * @test
+     */
+    public function getInstallments()
+    {
+        $this->assertInstanceOf(OneTimePaymentInstallments::class, $this->paymentMethod->getInstallments());
+    }
+
+    /**
+     * @test
+     */
+    public function forIssuerAsOneTimePayment()
+    {
+        $paymentMethod = PaymentMethod::forIssuerAsOneTimePayment($this->visa);
+
+        $this->assertSame(PaymentProduct::ONE_TIME_PAYMENT, $paymentMethod->getProduct());
+    }
+
+    /**
+     * @test
+     */
+    public function forIssuerWithInstallmentsByMerchant()
+    {
+        $paymentMethod = PaymentMethod::forIssuerWithInstallmentsByMerchant($this->visa, 8);
+
+        $this->assertSame(PaymentProduct::INSTALLMENTS_BY_AFFILIATED_MERCHANTS, $paymentMethod->getProduct());
+        $this->assertEquals('08', (string) $paymentMethod->getInstallments());
+    }
+
+    /**
+     * @test
+     */
+    public function forIssuerWithInstallmentsByCardIssuers()
+    {
+        $paymentMethod = PaymentMethod::forIssuerWithInstallmentsByCardIssuers($this->visa, 12);
+
+        $this->assertSame(PaymentProduct::INSTALLMENTS_BY_CARD_ISSUERS, $paymentMethod->getProduct());
+        $this->assertEquals('12', (string) $paymentMethod->getInstallments());
+    }
+}
