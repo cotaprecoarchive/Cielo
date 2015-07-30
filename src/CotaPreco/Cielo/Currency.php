ac5cffeaaf454d3035b4c583c4283c159e8502ed
Andrey <andreykvital@gmail.com>
2015-07-30 17:20:52 -0300

    Add Currency

---
+++ src/CotaPreco/Cielo/Currency.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace CotaPreco\Cielo;
+
+/**
+ * @author Andrey K. Vital <andreykvital@gmail.com>
+ * @link https://en.wikipedia.org/wiki/ISO_4217
+ */
+interface Currency
+{
+    /**
+     * @var int
+     */
+    const REAL = 986;
+
+    /**
+     * @var int
+     */
+    const EUR = 978;
+
+    /**
+     * @var int
+     */
+    const USD = 840;
+}
