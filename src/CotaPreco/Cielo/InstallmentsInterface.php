a9bf5657ebf4b14ae4f656430f786254ae8ca676
Andrey <andreykvital@gmail.com>
2015-07-30 16:51:21 -0300

    Add Installments & OneTimePaymentInstallments

---
+++ src/CotaPreco/Cielo/InstallmentsInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CotaPreco\Cielo;
+
+/**
+ * @author Andrey K. Vital <andreykvital@gmail.com>
+ */
+interface InstallmentsInterface
+{
+    /**
+     * @return string
+     */
+    public function __toString();
+}
