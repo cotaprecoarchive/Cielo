1627fe2a73945786aaa629015a7d7d0ad30192ce
Andrey <andreykvital@gmail.com>
2015-07-30 15:36:02 -0300

    Add CreditCard

---
+++ src/CotaPreco/Cielo/CreditCard.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CotaPreco\Cielo;
+
+/**
+ * @author Andrey K. Vital <andreykvital@gmail.com>
+ */
+interface CreditCard
+{
+    /**
+     * @var string
+     */
+    const VISA = 'visa';
+
+    /**
+     * @var string
+     */
+    const MASTERCARD = 'mastercard';
+}
