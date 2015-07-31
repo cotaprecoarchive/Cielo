34d099db8fbc1f6dfa7541e75ade3785b57d7753
Andrey <andreykvital@gmail.com>
2015-07-30 22:03:21 -0300

    Add CardHolder

---
+++ src/CotaPreco/Cielo/CardHolder.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * Copyright (c) 2015 Cota Preço
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace CotaPreco\Cielo;
+
+/**
+ * @author Andrey K. Vital <andreykvital@gmail.com>
+ */
+final class CardHolder implements IdentifiesHolder
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var CreditCard
+     */
+    private $card;
+
+    /**
+     * @param string     $name
+     * @param CreditCard $card
+     */
+    private function __construct($name, CreditCard $card)
+    {
+        $this->name = (string) $name;
+        $this->card = $card;
+    }
+
+    /**
+     * @param  string     $name
+     * @param  CreditCard $card
+     * @return self
+     */
+    public static function fromHolderNameAndCard($name, CreditCard $card)
+    {
+        return new self($name, $card);
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return CreditCard
+     */
+    public function getCard()
+    {
+        return $this->card;
+    }
+}
---
+++ tests/CotaPreco/Cielo/CardHolderTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace CotaPreco\Cielo;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * @author Andrey K. Vital <andreykvital@gmail.com>
+ */
+class CardHolderTest extends TestCase
+{
+    /**
+     * @var CardHolder
+     */
+    private $holder;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $this->holder = CardHolder::fromHolderNameAndCard('John Doe', CreditCard::createWithoutSecurityCode(
+            '5453010000066167',
+            CreditCardExpiration::fromYearAndMonth(2018, 5)
+        ));
+    }
+
+    /**
+     * @test
+     */
+    public function getName()
+    {
+        $this->assertEquals('John Doe', $this->holder->getName());
+    }
+
+    /**
+     * @test
+     */
+    public function getCard()
+    {
+        $this->assertInstanceOf(CreditCard::class, $this->holder->getCard());
+    }
+}
