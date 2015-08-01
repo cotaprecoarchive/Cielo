5551c71b780135c186bc8f1f7765594f1a237f20
Andrey <andreykvital@gmail.com>
2015-08-01 18:01:16 -0300

    Add `Transaction`

---
+++ src/CotaPreco/Cielo/Transaction.php
@@ -0,0 +1,381 @@
+<?php
+
+/*
+ * Copyright (c) 2015 Cota Preço
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace CotaPreco\Cielo;
+
+/**
+ * @author Andrey K. Vital <andreykvital@gmail.com>
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ * @SuppressWarnings(PHPMD.TooManyMethods)
+ */
+final class Transaction
+{
+    /**
+     * @var TransactionId
+     */
+    private $transactionId;
+
+    /**
+     * @var Pan
+     */
+    private $pan;
+
+    /**
+     * @var Order
+     */
+    private $order;
+
+    /**
+     * @var PaymentMethod
+     */
+    private $paymentMethod;
+
+    /**
+     * @var int
+     */
+    private $status;
+
+    /**
+     * @var Authentication|null
+     */
+    private $authentication;
+
+    /**
+     * @var Authorization|null
+     */
+    private $authorization;
+
+    /**
+     * @var Capture
+     */
+    private $capture;
+
+    /**
+     * @var Url|null
+     */
+    private $authenticationUrl;
+
+    /**
+     * @var TransactionWrappedToken|null
+     */
+    private $wrappedToken;
+
+    /**
+     * @var array|Cancellation[]
+     */
+    private $cancellations;
+
+    /**
+     * @param TransactionId                $transactionId
+     * @param Pan                          $pan
+     * @param Order                        $order
+     * @param PaymentMethod                $paymentMethod
+     * @param int                          $status
+     * @param Authentication|null          $authentication
+     * @param Authorization|null           $authorization
+     * @param Capture                      $capture
+     * @param Url|null                     $authenticationUrl
+     * @param TransactionWrappedToken|null $wrappedToken
+     * @param Cancellation[]               $cancellations
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     */
+    private function __construct(
+        TransactionId $transactionId,
+        Pan           $pan,
+        Order         $order,
+        PaymentMethod $paymentMethod,
+        $status,
+        Authentication          $authentication = null,
+        Authorization           $authorization = null,
+        Capture                 $capture = null,
+        Url                     $authenticationUrl = null,
+        TransactionWrappedToken $wrappedToken = null,
+        array $cancellations = []
+    ) {
+        $this->transactionId     = $transactionId;
+        $this->pan               = $pan;
+        $this->order             = $order;
+        $this->paymentMethod     = $paymentMethod;
+        $this->status            = (int) $status;
+        $this->authentication    = $authentication;
+        $this->authorization     = $authorization;
+        $this->capture           = $capture;
+        $this->authenticationUrl = $authenticationUrl;
+        $this->wrappedToken      = $wrappedToken;
+        $this->cancellations     = array_map(
+            function (Cancellation $cancellation) {
+                return $cancellation;
+            },
+            $cancellations
+        );
+    }
+
+    /**
+     * @param  TransactionId $transactionId
+     * @param  Pan           $pan
+     * @param  Order         $order
+     * @param  PaymentMethod $paymentMethod
+     * @param  int           $status
+     * @return self
+     */
+    public static function createFromRequiredComponents(
+        TransactionId $transactionId,
+        Pan $pan,
+        Order $order,
+        PaymentMethod $paymentMethod,
+        $status
+    ) {
+        return new self($transactionId, $pan, $order, $paymentMethod, $status);
+    }
+
+    /**
+     * @param  Authentication $authentication
+     * @return self
+     */
+    public function withAuthentication(Authentication $authentication)
+    {
+        return new self(
+            $this->transactionId,
+            $this->pan,
+            $this->order,
+            $this->paymentMethod,
+            $this->status,
+            $authentication,
+            $this->authorization
+        );
+    }
+
+    /**
+     * @param  Authorization $authorization
+     * @return self
+     */
+    public function withAuthorization(Authorization $authorization)
+    {
+        return new self(
+            $this->transactionId,
+            $this->pan,
+            $this->order,
+            $this->paymentMethod,
+            $this->status,
+            $this->authentication,
+            $authorization
+        );
+    }
+
+    /**
+     * @param  Capture $capture
+     * @return self
+     */
+    public function withCapture(Capture $capture)
+    {
+        return new self(
+            $this->transactionId,
+            $this->pan,
+            $this->order,
+            $this->paymentMethod,
+            $this->status,
+            $this->authentication,
+            $this->authorization,
+            $capture
+        );
+    }
+
+    /**
+     * @param  TransactionWrappedToken $token
+     * @return self
+     */
+    public function withWrappedToken(TransactionWrappedToken $token)
+    {
+        return new self(
+            $this->transactionId,
+            $this->pan,
+            $this->order,
+            $this->paymentMethod,
+            $this->status,
+            $this->authentication,
+            $this->authorization,
+            $this->capture,
+            $this->authenticationUrl,
+            $token
+        );
+    }
+
+
+    /**
+     * @param  Cancellation ...$cancellations
+     * @return Transaction
+     */
+    public function withCancellations(Cancellation ...$cancellations)
+    {
+        return new self(
+            $this->transactionId,
+            $this->pan,
+            $this->order,
+            $this->paymentMethod,
+            $this->status,
+            $this->authentication,
+            $this->authorization,
+            $this->capture,
+            $this->authenticationUrl,
+            $this->wrappedToken,
+            $cancellations
+        );
+    }
+
+    /**
+     * @return TransactionId
+     */
+    public function getTransactionId()
+    {
+        return $this->transactionId;
+    }
+
+    /**
+     * @return Pan
+     */
+    public function getPan()
+    {
+        return $this->pan;
+    }
+
+    /**
+     * @return Order
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+
+    /**
+     * @return PaymentMethod
+     */
+    public function getPaymentMethod()
+    {
+        return $this->paymentMethod;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasCapture()
+    {
+        return ! is_null($this->capture);
+    }
+
+    /**
+     * @return Capture|null
+     */
+    public function getCapture()
+    {
+        return $this->capture;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasAuthentication()
+    {
+        return ! is_null($this->authentication);
+    }
+
+    /**
+     * @return Authentication|null
+     */
+    public function getAuthentication()
+    {
+        return $this->authentication;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasAuthorization()
+    {
+        return ! is_null($this->authorization);
+    }
+
+    /**
+     * @return Authorization|null
+     */
+    public function getAuthorization()
+    {
+        return $this->authorization;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasWrappedToken()
+    {
+        return ! is_null($this->wrappedToken);
+    }
+
+    /**
+     * @return TransactionWrappedToken|null
+     */
+    public function getToken()
+    {
+        return $this->wrappedToken;
+    }
+
+    /**
+     * @return Url|null
+     */
+    public function getAuthenticationUrl()
+    {
+        return $this->authenticationUrl;
+    }
+
+    /**
+     * @return array|Cancellation[]
+     */
+    public function getCancellations()
+    {
+        return $this->cancellations;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isFullyCancelled()
+    {
+        return $this->status === TransactionStatus::CANCELLED;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasCancellations()
+    {
+        return count($this->cancellations) > 0;
+    }
+}
---
+++ tests/CotaPreco/Cielo/TransactionTest.php
@@ -0,0 +1,269 @@
+<?php
+
+namespace CotaPreco\Cielo;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * @author Andrey K. Vital <andreykvital@gmail.com>
+ */
+class TransactionTest extends TestCase
+{
+    /**
+     * @var Transaction
+     */
+    private $transaction;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $this->transaction = Transaction::createFromRequiredComponents(
+            TransactionId::fromString('10017348980735271001'),
+            Pan::fromTokenString('IqVz7P9zaIgTYdU41HaW/OB/d7Idwttqwb2vaTt8MT0='),
+            Order::fromPreviouslyIssuedOrder(
+                '1196683550',
+                '1000',
+                new \DateTimeImmutable('2011-12-08T10:44:24.244-02:00')
+            ),
+            PaymentMethod::forIssuerAsOneTimePayment(
+                CardIssuer::fromCreditCardType(CreditCardType::VISA)
+            ),
+            TransactionStatus::CREATED
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getTransactionId()
+    {
+        $this->assertInstanceOf(TransactionId::class, $this->transaction->getTransactionId());
+    }
+
+    /**
+     * @test
+     */
+    public function getPan()
+    {
+        $this->assertInstanceOf(Pan::class, $this->transaction->getPan());
+    }
+
+    /**
+     * @test
+     */
+    public function getOrder()
+    {
+        $this->assertInstanceOf(Order::class, $this->transaction->getOrder());
+    }
+
+    /**
+     * @test
+     */
+    public function getPaymentMethod()
+    {
+        $this->assertInstanceOf(PaymentMethod::class, $this->transaction->getPaymentMethod());
+    }
+
+    public function testGetStatus()
+    {
+        $this->assertSame(TransactionStatus::CREATED, $this->transaction->getStatus());
+    }
+
+    /**
+     * @test
+     */
+    public function hasAuthentication()
+    {
+        $this->assertFalse($this->transaction->hasAuthentication());
+    }
+
+    /**
+     * @test
+     */
+    public function getAuthentication()
+    {
+        $this->assertNull($this->transaction->getAuthentication());
+    }
+
+    /**
+     * @test
+     */
+    public function withAuthentication()
+    {
+        $transaction = $this->transaction->withAuthentication(
+            new Authentication(
+                2,
+                'Autenticada com sucesso',
+                new \DateTimeImmutable('2011-12-08T10:44:47.311-02:00'),
+                1000,
+                Eci::fromIndicator(5)
+            )
+        );
+
+        $this->assertNotSame($transaction, $this->transaction);
+        $this->assertTrue($transaction->hasAuthentication());
+        $this->assertInstanceOf(Authentication::class, $transaction->getAuthentication());
+    }
+
+    /**
+     * @test
+     */
+    public function hasAuthorization()
+    {
+        $this->assertFalse($this->transaction->hasAuthorization());
+    }
+
+    /**
+     * @test
+     */
+    public function getAuthorization()
+    {
+        $this->assertNull($this->transaction->getAuthorization());
+    }
+
+    /**
+     * @test
+     */
+    public function withAuthorization()
+    {
+        $transaction = $this->transaction->withAuthorization(
+            new Authorization(
+                5,
+                'Autorização negada',
+                new \DateTimeImmutable('2011-12-09T10:58:45.847-02:00'),
+                1000,
+                '57',
+                '221766'
+            )
+        );
+
+        $this->assertNotSame($transaction, $this->transaction);
+        $this->assertTrue($transaction->hasAuthorization());
+        $this->assertInstanceOf(Authorization::class, $transaction->getAuthorization());
+    }
+
+    /**
+     * @test
+     */
+    public function hasWrappedToken()
+    {
+        $this->assertFalse($this->transaction->hasWrappedToken());
+    }
+
+    /**
+     * @test
+     */
+    public function getToken()
+    {
+        $this->assertNull($this->transaction->getToken());
+    }
+
+    /**
+     * @test
+     */
+    public function withWrappedToken()
+    {
+        $transaction = $this->transaction->withWrappedToken(new TransactionWrappedToken(
+            CardToken::fromString('TuS6LeBHWjqFFtE7S3zR052Jl/KUlD+tYJFpAdlA87E='),
+            GeneratedTokenStatus::UNBLOCKED,
+            '455187******0183'
+        ));
+
+        $this->assertNotSame($transaction, $this->transaction);
+        $this->assertTrue($transaction->hasWrappedToken());
+        $this->assertInstanceOf(TransactionWrappedToken::class, $transaction->getToken());
+    }
+
+    /**
+     * @test
+     */
+    public function hasCapture()
+    {
+        $this->assertFalse($this->transaction->hasCapture());
+    }
+
+    /**
+     * @test
+     */
+    public function getCapture()
+    {
+        $this->assertNull($this->transaction->getCapture());
+    }
+
+    /**
+     * @test
+     */
+    public function withCapture()
+    {
+        $transaction = $this->transaction->withCapture(
+            new Capture(
+                6,
+                'Transacao capturada com sucesso',
+                new \DateTimeImmutable('2011-12-08T14:23:08.779-02:00'),
+                900,
+                900
+            )
+        );
+
+        $this->assertNotSame($transaction, $this->transaction);
+        $this->assertTrue($transaction->hasCapture());
+        $this->assertInstanceOf(Capture::class, $transaction->getCapture());
+    }
+
+    /**
+     * @test
+     */
+    public function getAuthenticationUrl()
+    {
+        $this->assertNull($this->transaction->getAuthenticationUrl());
+    }
+
+    /**
+     * @test
+     */
+    public function getCancellations()
+    {
+        $this->assertCount(0, $this->transaction->getCancellations());
+    }
+
+    /**
+     * @test
+     */
+    public function hasCancellations()
+    {
+        $this->assertFalse($this->transaction->hasCancellations());
+    }
+
+    /**
+     * @test
+     */
+    public function isFullyCancelled()
+    {
+        $this->assertFalse($this->transaction->isFullyCancelled());
+    }
+
+    /**
+     * @test
+     */
+    public function withCancellations()
+    {
+        $cancellations = [];
+
+        for ($i = 0; $i < 5; $i += 1) {
+            $cancellations[] = new Cancellation(
+                9,
+                'Transacao cancelada com sucesso',
+                new \DateTimeImmutable('2011-12-08T16:46:35.109-02:00'),
+                1000
+            );
+        }
+
+        $transaction = $this->transaction->withCancellations(...$cancellations);
+
+        $this->assertNotSame($transaction, $this->transaction);
+        $this->assertCount(5, $transaction->getCancellations());
+        $this->assertTrue($transaction->hasCancellations());
+    }
+}
