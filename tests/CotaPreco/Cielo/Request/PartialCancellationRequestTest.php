98571c307c816fe167eb18d55dce3adc9a72d101
Andrey <andreykvital@gmail.com>
2015-08-01 09:41:40 -0300

    Add PartialCancellationRequestTest

---
+++ tests/CotaPreco/Cielo/Request/PartialCancellationRequestTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace CotaPreco\Cielo\Request;
+
+use CotaPreco\Cielo\Merchant;
+use CotaPreco\Cielo\TransactionId;
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * @author Andrey K. Vital <andreykvital@gmail.com>
+ */
+class PartialCancellationRequestTest extends TestCase
+{
+    /**
+     * @var PartialCancellationRequest
+     */
+    private $request;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $merchant = Merchant::fromAffiliationIdAndKey(
+            getenv('CIELO_AFFILIATION_ID'),
+            getenv('CIELO_AFFILIATION_KEY')
+        );
+
+        $this->request = new PartialCancellationRequest(
+            TransactionId::fromString('12345678910111213141'),
+            $merchant,
+            1000
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getMerchant()
+    {
+        $this->assertInstanceOf(Merchant::class, $this->request->getMerchant());
+    }
+
+    /**
+     * @test
+     */
+    public function getTransactionId()
+    {
+        $this->assertInstanceOf(TransactionId::class, $this->request->getTransactionId());
+    }
+
+    /**
+     * @test
+     */
+    public function getValue()
+    {
+        $this->assertEquals(1000, $this->request->getValue());
+    }
+}
