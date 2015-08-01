054f56bb70e50388b1c934263d4e5ce4b4ebc2ce
Andrey <andreykvital@gmail.com>
2015-08-01 20:21:11 -0300

    Ensure three or four digits on CVV

--- tests/CotaPreco/Cielo/CvvTest.php
+++ tests/CotaPreco/Cielo/CvvTest.php
@@ -12,8 +12,37 @@ class CvvTest extends TestCase
     /**
      * @test
      */
-    public function fromString()
+    public function fromVerificationValue()
     {
-        $this->assertEquals('1234', (string) Cvv::fromString('1234'));
+        $this->assertEquals('1234', (string) Cvv::fromVerificationValue('1234'));
+    }
+
+    /**
+     * @test
+     * @param mixed $value
+     * @dataProvider provideInvalidVerificationValues
+     */
+    public function throwsInvalidArgument($value)
+    {
+        $this->setExpectedException(\InvalidArgumentException::class);
+
+        Cvv::fromVerificationValue($value);
+    }
+
+    /**
+     * @return array
+     */
+    public function provideInvalidVerificationValues()
+    {
+        return [
+            [''],
+            [null],
+            [1],
+            [12],
+            [99992],
+            [-1],
+            [true],
+            [-1202]
+        ];
     }
 }
