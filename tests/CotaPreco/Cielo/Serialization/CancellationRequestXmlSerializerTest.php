beac1d2f5de42360da0e0f2240059a7ac969c902
Andrey <andreykvital@gmail.com>
2015-08-01 13:06:47 -0300

    Add SearchTransactionRequestTest & CancellationRequestXmlSerializerTest

---
+++ tests/CotaPreco/Cielo/Serialization/CancellationRequestXmlSerializerTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace CotaPreco\Cielo\Serialization;
+
+use CotaPreco\Cielo\Merchant;
+use CotaPreco\Cielo\Request\CancellationRequestInterface;
+use CotaPreco\Cielo\Request\FullCancellationRequest;
+use CotaPreco\Cielo\Request\PartialCancellationRequest;
+use CotaPreco\Cielo\TransactionId;
+
+/**
+ * @author Andrey K. Vital <andreykvital@gmail.com>
+ */
+class CancellationRequestXmlSerializerTest extends XmlSerializerTestCase
+{
+    /**
+     * @return array
+     */
+    public function provideCancellationRequests()
+    {
+        $transactionId = TransactionId::fromString('10017348980735271001');
+
+        $merchant = Merchant::fromAffiliationIdAndKey(
+            getenv('CIELO_AFFILIATION_ID'),
+            getenv('CIELO_AFFILIATION_KEY')
+        );
+
+        return [
+            [
+                new FullCancellationRequest($transactionId, $merchant),
+                <<<XML
+<requisicao-cancelamento>
+    <tid>10017348980735271001</tid>
+    <dados-ec>
+        <numero>1006993069</numero>
+        <chave>25fbb99741c739dd84d7b06ec78c9bac718838630f30b112d033ce2e621b34f3</chave>
+    </dados-ec>
+</requisicao-cancelamento>
+XML
+            ],
+            [
+                new PartialCancellationRequest(
+                    $transactionId,
+                    $merchant,
+                    50000
+                ),
+                <<<XML
+<requisicao-cancelamento>
+    <tid>10017348980735271001</tid>
+    <dados-ec>
+        <numero>1006993069</numero>
+        <chave>25fbb99741c739dd84d7b06ec78c9bac718838630f30b112d033ce2e621b34f3</chave>
+    </dados-ec>
+    <valor>50000</valor>
+</requisicao-cancelamento>
+XML
+            ],
+            [
+                new PartialCancellationRequest(
+                    $transactionId,
+                    $merchant,
+                    1000
+                ),
+                <<<XML
+<requisicao-cancelamento>
+    <tid>10017348980735271001</tid>
+    <dados-ec>
+        <numero>1006993069</numero>
+        <chave>25fbb99741c739dd84d7b06ec78c9bac718838630f30b112d033ce2e621b34f3</chave>
+    </dados-ec>
+    <valor>1000</valor>
+</requisicao-cancelamento>
+XML
+            ]
+        ];
+    }
+
+    /**
+     * @test
+     * @param CancellationRequestInterface $request
+     * @param string                       $xml
+     * @dataProvider provideCancellationRequests
+     */
+    public function invoke(CancellationRequestInterface $request, $xml)
+    {
+        $serializer = new CancellationRequestXmlSerializer();
+
+        $this->assertXmlRequestEquals(
+            $xml,
+            $serializer($request)
+        );
+    }
+}
---
+++ tests/CotaPreco/Cielo/Serialization/XmlSerializerTestCase.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace CotaPreco\Cielo\Serialization;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * @author Andrey K. Vital <andreykvital@gmail.com>
+ */
+abstract class XmlSerializerTestCase extends TestCase
+{
+    /**
+     * @param string $expected
+     * @param string $request
+     */
+    protected function assertXmlRequestEquals($expected, $request)
+    {
+        $this->assertXmlStringEqualsXmlString(
+            $this->removeRootAttributes($expected),
+            $this->removeRootAttributes($request)
+        );
+    }
+
+    /**
+     * @param  string $xml
+     * @return string
+     */
+    protected function removeRootAttributes($xml)
+    {
+        $document = new \DOMDocument();
+
+        $document->loadXML($xml);
+
+        $root = $document->documentElement;
+
+        while ($root->attributes->length) {
+            $root->removeAttribute($root->attributes->item(0)->nodeName);
+        }
+
+        return $document->saveXML();
+    }
+}
