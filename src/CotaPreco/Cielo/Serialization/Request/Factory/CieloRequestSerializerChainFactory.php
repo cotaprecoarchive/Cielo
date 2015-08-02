e92cc633527b4b188e989c9e1caee90b99de991a
Andrey <andreykvital@gmail.com>
2015-08-02 12:54:17 -0300

    Add CieloRequestSerializerChainFactory

---
+++ src/CotaPreco/Cielo/Serialization/Request/Factory/CieloRequestSerializerChainFactory.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * Copyright (c) 2015 Cota Preço
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace CotaPreco\Cielo\Serialization\Request\Factory;
+
+use CotaPreco\Cielo\Serialization\Request\SerializerChain;
+use CotaPreco\Cielo\Serialization\Request\Xml\AuthorizeTransactionSerializer;
+use CotaPreco\Cielo\Serialization\Request\Xml\CancellationSerializer;
+use CotaPreco\Cielo\Serialization\Request\Xml\CaptureSerializer;
+use CotaPreco\Cielo\Serialization\Request\Xml\CreateTokenForHolderSerializer;
+use CotaPreco\Cielo\Serialization\Request\Xml\CreateTransactionSerializer;
+use CotaPreco\Cielo\Serialization\Request\Xml\SearchTransactionSerializer;
+
+/**
+ * @author Andrey K. Vital <andreykvital@gmail.com>
+ */
+final class CieloRequestSerializerChainFactory
+{
+    /**
+     * @return SerializerChain
+     */
+    public function __invoke()
+    {
+        return new SerializerChain(
+            new SearchTransactionSerializer(),
+            new AuthorizeTransactionSerializer(),
+            new CaptureSerializer(),
+            new CreateTokenForHolderSerializer(),
+            new CancellationSerializer(),
+            new CreateTransactionSerializer()
+        );
+    }
+}
