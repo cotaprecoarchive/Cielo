03338cc91944cbe55a709cc685c518b35be54f3c
Andrey <andreykvital@gmail.com>
2015-08-04 13:14:52 -0300

    Ensure not empty

--- tests/CotaPreco/Cielo/CardTokenTest.php
+++ tests/CotaPreco/Cielo/CardTokenTest.php
@@ -19,4 +19,28 @@ class CardTokenTest extends TestCase
             CardToken::fromString('TuS6LeBHWjqFFtE7S3zR052Jl/KUlD+tYJFpAdlA87E=')
         );
     }
+
+    /**
+     * @test
+     * @param string $tokenString
+     * @dataProvider invalidTokens
+     */
+    public function throwsInvalidArgumentException($tokenString)
+    {
+        $this->setExpectedException(\InvalidArgumentException::class);
+
+        CardToken::fromString($tokenString);
+    }
+
+    /**
+     * @return \string[][]
+     */
+    public function invalidTokens()
+    {
+        return [
+            [''],
+            [null],
+            [hash('sha512', 'foo')]
+        ];
+    }
 }
